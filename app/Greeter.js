164ef403a5d8b78c71ce4eecd78c1a9034d8f3fa
wangwz <wangwz@topwalk.com>
2017-07-17 18:04:21 +0800

    webpack学习练习，使用的时候讲其克隆下来，cmd进入此目录npm install安装环境

---
+++ app/Greeter.js
@@ -0,0 +1,7 @@
+module.exports=function test(){
+	var a=5;
+	var b=6;
+	var greet = document.createElement("div");
+	greet.textContent = "Hi x and greetings我是王维璋你"+"<h3>sss</h3>";
+	return greet;
+}
